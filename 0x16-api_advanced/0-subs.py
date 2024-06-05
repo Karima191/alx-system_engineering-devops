40c59609ba28b601a96d3a8dc80fcd75663f6b9b
“Karima191” <karima.sel91@gmail.com>
2024-06-05 08:23:29 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
@@ -16,25 +15,3 @@ def number_of_subscribers(subreddit):
             return 0
         results = response.json().get("data")
         return results.get("subscribers")
-
-        response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
-
-        data = response.json().get("data")
-        if data is None:
-            return 0
-
-        return data.get("subscribers", 0)  # Return 0 if "subscribers" is not found
-
-    except requests.exceptions.HTTPError as http_err:
-        return 0
-    except requests.exceptions.RequestException as req_err:
-        return 0
-    except ValueError as json_err:
-        return 0
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-    else:
-        print("Please pass an argument for the subreddit to search.")
